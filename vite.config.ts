cc4dd54afd16b5ba31516ea89be888205bfd6b4b
Yonatan Kra <me@yonatankra.com>
2024-12-21 21:19:34 +0200

    fix(heart): release tag

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vitest/config'
 import { resolve } from 'path';
 import fs from 'fs';
+import { execSync } from 'child_process';
 
 const isLibrary = process.env.BUILD_LIB === 'true'
 
@@ -9,39 +10,36 @@ function getPackageJSON(filePath) {
   return JSON.parse(fs.readFileSync(resolve(filePath), 'utf-8'));
 }
 
-function getLatestVersion(packageJson) {
+function getLatestVersion() {
   try {
-    if (packageJson.version) {
-      const [major, minor, patch] = packageJson.version.split('.').map(Number);
-      const updateType = process.env.UPDATE_TYPE || 'patch';
+    const result = execSync('npm view @yonatan-kra/heart-component version').toString().trim();
+    const [major, minor, patch] = result.split('.').map(Number);
+    const updateType = process.env.UPDATE_TYPE || 'patch';
 
-      switch (updateType) {
-        case 'major':
-          return `${major + 1}.0.0`;
-        case 'minor':
-          return `${major}.${minor + 1}.0`;
-        case 'patch':
-        default:
-          return `${major}.${minor}.${patch + 1}`;
-      }
-    } else {
-      return '1.0.0';
+    switch (updateType) {
+      case 'major':
+        return `${major + 1}.0.0`;
+      case 'minor':
+        return `${major}.${minor + 1}.0`;
+      case 'patch':
+      default:
+        return `${major}.${minor}.${patch + 1}`;
     }
   } catch (error) {
     return '1.0.0';
   }
 }
 
+
 function generatePackageJson() {
   try {
     const packageJson = getPackageJSON('./src/components/heart/package.json');
-    packageJson.version = getLatestVersion(packageJson);
+    packageJson.version = getLatestVersion();
 
     if (!fs.existsSync('./dist/heart')) {
       fs.mkdirSync('./dist/heart', { recursive: true })
     }
     fs.writeFileSync('./dist/heart/package.json', JSON.stringify(packageJson, null, 2))
-    fs.writeFileSync('./src/components/heart/package.json', JSON.stringify(packageJson, null, 2))
   } catch (error) {
     console.error(error);
   }
@@ -50,7 +48,6 @@ function generatePackageJson() {
 function copyReadme() {
   console.log('Readme copy');
   try {
-    // Move README.md
     const readmeSrc = './src/components/heart/README.md';
     const readmeDest = './dist/heart/README.md';
     if (fs.existsSync(readmeSrc)) {
